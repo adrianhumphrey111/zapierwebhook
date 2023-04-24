0506d51c59a13e4023259a255f1ad1ccd9428e77
adrianhumphrey111 <adrianhumphrey374@gmail.com>
2023-04-24 14:35:38 -0700

    Use json.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
